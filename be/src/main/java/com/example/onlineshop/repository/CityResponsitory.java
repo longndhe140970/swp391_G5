e5bc39b418193549a68a2246036f0289927a1eb5
longndhe <quanthienlong21@gmail.com>
2024-06-05 10:34:01 +0700

    #51 fixed cart be

--- be/src/main/java/com/example/onlineshop/repository/CityResponsitory.java
+++
@@ -1,9 +0,0 @@
-package com.example.onlineshop.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CityResponsitory extends JpaRepository<City, Integer> {
-    City findByName(String name);
-}
