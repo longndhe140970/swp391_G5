daba88fc8e2e0d6fecb6a5827be6ff70a8e65ec7
LeLoc1712 <loclvvhe180443@fpt.edu.vn>
2024-06-26 15:50:08 +0700

    #17

--- be/src/main/java/com/example/onlineshop/payload/request/AuthorRequest.java
+++ be/src/main/java/com/example/onlineshop/payload/request/AuthorRequest.java
@@ -1,5 +1,16 @@
 package com.example.onlineshop.payload.request;
 
-public class AuthorRequest {
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthorRequest {
+	String name;
 }
